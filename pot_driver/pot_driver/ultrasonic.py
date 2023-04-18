16f9a241759f7ced47e61594a7de81a387546cf9
Liam <68168753+lee-yumm@users.noreply.github.com>
2023-04-17 21:46:01 -0400

    Add files via upload

---
+++ pot_driver/pot_driver/ultrasonic.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+import time
+import RPi.GPIO as GPIO
+#from geometry_msgs.msg import Twist
+from std_msgs.msg import Float32
+
+class UltrasonicNode(Node):
+    def __init__(self):
+        super().__init__('ultrasonic_node')
+        self.publisher_ = self.create_publisher(Float32, 'ultrasonic', 10)
+        self.trigger_pin = 18
+        self.echo_pin = 24
+        self.valve_pin = 16
+        self.min_distance = 6  # in cm
+        self.timeout = 0.1  # in seconds
+
+        # Initialize GPIO pins
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.trigger_pin, GPIO.OUT)
+        GPIO.setup(self.echo_pin, GPIO.IN)
+        GPIO.setup(self.valve_pin, GPIO.OUT)
+        GPIO.output(self.valve_pin, GPIO.LOW)
+
+        # Initialize cmd_vel subscriber
+        #self.cmd_vel_subscriber = self.create_subscription(Twist, 'cm_vel', self.cmd_vel_callback, 10)
+
+        self.timer = self.create_timer(0.1, self.timer_callback)
+
+    def timer_callback(self):
+        # Send trigger pulse
+        GPIO.output(self.trigger_pin, GPIO.HIGH)
+        time.sleep(0.00001)
+        GPIO.output(self.trigger_pin, GPIO.LOW)
+
+        # Wait for echo pulse
+        pulse_start = time.time()
+        pulse_end = time.time()
+        while GPIO.input(self.echo_pin) == GPIO.LOW:
+            pulse_start = time.time()
+            if pulse_start - pulse_end > self.timeout:
+                break
+        while GPIO.input(self.echo_pin) == GPIO.HIGH:
+            pulse_end = time.time()
+            if pulse_end - pulse_start > self.timeout:
+                break
+
+        # Calculate distance in cm
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
+
+        # Close valve by default
+        GPIO.output(self.valve_pin, GPIO.LOW)
+        self.get_logger().info('Vavle is closed')
+
+        # Open valve if distance is less than minimum
+        if distance > self.min_distance:
+            GPIO.output(self.valve_pin, GPIO.HIGH)
+            self.get_logger().info('Vavle is open')
+
+        # Create message with distance variable
+        msg = Float32()
+        msg.data = distance
+        self.publisher_.publish(msg)
+
+        # Print distance to console
+        # self.get_logger().info('Distance: {} cm'.format(distance))
+        self.get_logger().info('Publishing: %.2fcm' % msg.data)
+
+    def __del__(self):
+        # Cleanup GPIO pins
+        GPIO.cleanup()
+
+def main(args=None):
+    rclpy.init(args=args)
+    ultrasonic_node = UltrasonicNode()
+    rclpy.spin(ultrasonic_node)
+    ultrasonic_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ thepotbot/launch/drive.launch.py
@@ -0,0 +1,127 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='thepotbot'
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+    )
+
+
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
+
+
+
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
+    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controller.yaml')
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description},
+                    controller_params_file]
+    )
+
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
+        )
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
+    )
+
+    ldlidar = Node(
+      package='ldlidar_sl_ros2',
+      executable='ldlidar_sl_ros2_node',
+      name='ldlidar_publisher_ld14',
+      output='screen',
+      parameters=[
+        {'product_name': 'LDLiDAR_LD14'},
+        {'laser_scan_topic_name': 'scan'},
+        {'point_cloud_2d_topic_name': 'pointcloud2d'},
+        {'frame_id': 'base_laser'},
+        {'port_name': '/dev/ttyUSB0'},
+        {'serial_baudrate' : 115200},
+        {'laser_scan_dir': True},
+        {'enable_angle_crop_func': False},
+        {'angle_crop_min': 135.0},
+        {'angle_crop_max': 225.0}
+      ]
+    )
+
+
+    # Code for delaying a node (I haven't tested how effective it is)
+    # 
+    # First add the below lines to imports
+    # from launch.actions import RegisterEventHandler
+    # from launch.event_handlers import OnProcessExit
+    #
+    # Then add the following below the current diff_drive_spawner
+    # delayed_diff_drive_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=spawn_entity,
+    #         on_exit=[diff_drive_spawner],
+    #     )
+    # )
+    #
+    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
+
+
+
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+        ldlidar,
+        twist_mux,
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner
+    ])
