3a1e25be9120033b1a3e613ad20a9636ed24311f
Liam <68168753+lee-yumm@users.noreply.github.com>
2023-05-13 18:49:29 -0400

    Update sim.launch.py

--- thepotbot/launch/sim.launch.py
+++ thepotbot/launch/sim.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    package_name='thepotbot' #<--- CHANGE ME
+    package_name='thepotbot' # Robot package name
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -32,11 +32,23 @@ def generate_launch_description():
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+    spawn_entity = Node(package='gazebo_ros',
+                        executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'ayo'],
                         output='screen')
+    
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
 
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
 
     # Code for delaying a node (I haven't tested how effective it is)
     # 
@@ -60,5 +72,7 @@ def generate_launch_description():
     return LaunchDescription([
         rsp,
         gazebo,
-        spawn_entity
+        spawn_entity,
+        diff_driver_spawner,
+        joint_broad_spawner
     ])
